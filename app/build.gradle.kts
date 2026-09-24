a54ebc31896292ee5d7d02f2ee7289f859ab92f3
dsgs1987 <dsgs1987@gmail.com>
2026-09-25 01:13:32 +0300

    fix Kotlin and Java JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,13 @@ plugins {
 }
 android { namespace = "ru.trudyaga.music"; compileSdk = 35
     defaultConfig { applicationId = "ru.trudyaga.music"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
     buildFeatures { compose = true }
 }
 dependencies {
